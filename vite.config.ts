2be3e8cb6a119064b1c73bd41e68ae3f164ce4e1
mukherjeeaanish-oss <mukherjeeaanish@gmail.com>
2026-07-15 10:54:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/vite-plugin-react' // or vue/svelte depending on your framework
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Final-Kiosk-App/', // <-- Add this exact line right here!
 })
